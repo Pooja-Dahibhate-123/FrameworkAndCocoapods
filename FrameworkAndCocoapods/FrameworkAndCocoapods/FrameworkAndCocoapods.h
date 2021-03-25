6850ed1e32d9fd4082694afad3d31266ef583b25
Techbulls <apple@Apples-MacBook-Pro.local>
2021-03-25 18:37:31 +0530

    finished framework

---
+++ FrameworkAndCocoapods/FrameworkAndCocoapods/FrameworkAndCocoapods.h
@@ -0,0 +1,18 @@
+//
+//  FrameworkAndCocoapods.h
+//  FrameworkAndCocoapods
+//
+//  Created by Techbulls on 25/03/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FrameworkAndCocoapods.
+FOUNDATION_EXPORT double FrameworkAndCocoapodsVersionNumber;
+
+//! Project version string for FrameworkAndCocoapods.
+FOUNDATION_EXPORT const unsigned char FrameworkAndCocoapodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FrameworkAndCocoapods/PublicHeader.h>
+
+
---
+++ FrameworkAndCocoapods/FrameworkAndCocoapods/Service.swift
@@ -0,0 +1,17 @@
+//
+//  Service.swift
+//  FrameworkAndCocoapods
+//
+//  Created by Techbulls on 25/03/21.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init(){}
+    
+    public static func doSomething() -> String{
+        return "Hello world"
+    }
+}
